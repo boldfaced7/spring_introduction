8da4dcae3eca0c25acfc37f9a93eaff7ccb7ca50
boldfaced7 <boldfaced753@gmail.com>
2023-04-25 21:53:40 +0900

    웹에서 파라미터(name)를 받아 만든 문자열을 Response Body에 넣는 hello-string 메소드 작성

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -16,5 +17,10 @@ public class HelloController {
     public String helloMvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name", name);
         return "hello-mvc";
+    }
+    @GetMapping("hello-string")
+    @ResponseBody // http 통신 프로토콜에는 헤더와 바디가 존재하는데, 응답 바디에 해당 메소드의 반환값을 데이터를 직접 넣어주겠다는 것
+    public String helloString(@RequestParam("name") String name) {
+        return "hello + " + name;
     }
 }

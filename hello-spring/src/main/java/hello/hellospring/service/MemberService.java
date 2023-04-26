76f420a1df563cababaefd4c4afef6eff6d3226a
boldfaced7 <boldfaced753@gmail.com>
2023-04-26 13:53:41 +0900

    전체 회원 데이터를 반환하는 findMembers() 메소드 작성

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -4,19 +4,22 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepositroy;
 import hello.hellospring.repository.MemoryMemberRepository;
 
+import java.util.List;
+
 public class MemberService {
     private final MemberRepositroy memberRepositroy = new MemoryMemberRepository();
-
     public Long join(Member member) {
         validateDuplicatedMember(member);
         memberRepositroy.save(member);
         return member.getId();
     }
-
     private void validateDuplicatedMember(Member member) {
         memberRepositroy.findByName(member.getName())
                 .ifPresent(m -> {
                     throw new IllegalStateException("이미 존재하는 회원입니다.");
                 });
+    }
+    public List<Member> findMembers() {
+        return memberRepositroy.findAll();
     }
 }

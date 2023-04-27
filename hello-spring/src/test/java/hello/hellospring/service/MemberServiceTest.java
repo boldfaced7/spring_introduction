49b134f9f4452ff6bdae91aef59ccda478deeebb
boldfaced7 <boldfaced753@gmail.com>
2023-04-27 13:35:41 +0900

    MemoryMemberRepository 객체를 생성해, MemberRepository를 받는 MemberService 생성자에 넣어주는 beforeEach() 메소드를 작성하고, @BeforeEach 어노테이션을 붙임

--- hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -2,18 +2,19 @@ package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemoryMemberRepository;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 
 class MemberServiceTest {
-    MemberService memberService = new MemberService();
-    MemoryMemberRepository memberRepository = new MemoryMemberRepository();
-    @AfterEach
-    public void afterEach() {
-        memberRepository.clearStore();
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+    @BeforeEach
+    public void beforeEach() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
     }
     @Test
     public void 회원가입() {

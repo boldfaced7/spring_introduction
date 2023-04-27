a822fd738cd89edc347f88046abde619a7eb5356
준모 강 <boldfaced7@hanyang.ac.kr>
2023-04-27 20:14:10 +0900

    JdbcMemberRepository 구현체를 사용하기 위해, SpringConfig 클래스 코드 변경

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,16 +1,26 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepositroy;
-import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberRepositroy memberRepositroy() {
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
     @Bean
     public MemberService memberService() {

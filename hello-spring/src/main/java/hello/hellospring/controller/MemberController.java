8a8281cdd9ab17067f6aa11935c9cf1693687335
준모 강 <boldfaced7@hanyang.ac.kr>
2023-04-27 16:22:52 +0900

    MemberController에 /members/new에 맞춰 회원 등록 form을 출력하기 위해, createForm() 메소드를 작성

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -3,20 +3,17 @@ package hello.hellospring.controller;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MemberController {
-    /*
-    ///@Autowired private MemberService memberService;
-    private MemberService memberService;
-    @Autowired
-    public void setMemberService(MemberService memberService) {
-        this.memberService = memberService;
-    }
-     */
     private final MemberService memberService;
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
     }
 }

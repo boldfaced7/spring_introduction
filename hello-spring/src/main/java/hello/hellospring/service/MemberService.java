f4a32d4cd2debe62500929a238585162599e126f
준모 강 <boldfaced7@hanyang.ac.kr>
2023-04-27 15:52:52 +0900

    MemberService와 MemoryMemberRepository에 각각 @Service, @Repository 어노테이션을 붙이고, MemberService의 생성자에 @Autowired 어노테이션을 붙임

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -2,12 +2,15 @@ package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepositroy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-
+@Service
 public class MemberService {
     private final MemberRepositroy memberRepositroy;
+    @Autowired
     public MemberService(MemberRepositroy memberRepositroy) {
         this.memberRepositroy = memberRepositroy;
     }

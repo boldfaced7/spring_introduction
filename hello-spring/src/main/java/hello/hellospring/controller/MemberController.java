dd0959f75f712a1446d8b529cc3a33cfa9e97f04
준모 강 <boldfaced7@hanyang.ac.kr>
2023-04-27 16:09:03 +0900

    필드 주입 방식 구현

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -6,9 +6,5 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class MemberController {
-    private final MemberService memberService;
-    @Autowired
-    public MemberController(MemberService memberService) {
-        this.memberService = memberService;
-    }
+    @Autowired private MemberService memberService;
 }
